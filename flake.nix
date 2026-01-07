a0cd8ca6bd25d9efb04e1145062f7433a1f52762
Patrick Jackson <patrick@jackson.dev>
2026-01-07 13:47:09 -0800

    fix(container): add non-root user, LICENSE, SBOM attestations + cleanups

--- flake.nix
+++ flake.nix
@@ -149,6 +149,33 @@
         # Main package
         eidetica = eidetica-bin;
 
+        # Non-root user setup for container security
+        # Creates passwd/group/shadow files for the eidetica user (UID/GID 1000)
+        nonRootUserSetup = let
+          user = "eidetica";
+          uid = "1000";
+          gid = "1000";
+        in [
+          (pkgs.writeTextDir "etc/passwd" ''
+            root:x:0:0:root:/root:/bin/false
+            ${user}:x:${uid}:${gid}::/${user}:
+          '')
+          (pkgs.writeTextDir "etc/group" ''
+            root:x:0:
+            ${user}:x:${gid}:
+          '')
+          (pkgs.writeTextDir "etc/shadow" ''
+            root:!x:::::::
+            ${user}:!:::::::
+          '')
+        ];
+
+        # License file for container compliance
+        licenseFile = pkgs.runCommand "license" {} ''
+          mkdir -p $out
+          cp ${./LICENSE.txt} $out/LICENSE
+        '';
+
         # OCI container image
         eidetica-image = pkgs.dockerTools.buildImage {
           name = "eidetica";
@@ -157,12 +184,13 @@
 
           copyToRoot = pkgs.buildEnv {
             name = "image-root";
-            paths = [eidetica-bin];
-            pathsToLink = ["/bin"];
+            paths = [eidetica-bin licenseFile] ++ nonRootUserSetup;
+            pathsToLink = ["/bin" "/etc" "/"];
           };
 
           config = {
             Cmd = ["${eidetica-bin}/bin/eidetica"];
+            User = "1000:1000";
             WorkingDir = "/data";
             ExposedPorts = {
               "3000/tcp" = {};
@@ -173,6 +201,7 @@
             Env = [
               "EIDETICA_DATA_DIR=/data"
               "EIDETICA_HOST=0.0.0.0"
+              "HOME=/tmp"
             ];
             Labels = {
               "org.opencontainers.image.source" = "https://github.com/arcuru/eidetica";
